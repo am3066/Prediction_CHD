e7df7a256883410a1ec6594ba6312d42aa52467d
am3066 <itsihssaname@gmail.com>
2025-11-30 20:03:20 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
-#import pandas as pd
-#import numpy as np
-#import pickle
+import pandas as pd
+import numpy as np
+import pickle
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
